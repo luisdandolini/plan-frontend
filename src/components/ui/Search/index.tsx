599d066a04b9ef7944450c819ee3f9b64ed54cff
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2025-05-20 11:19:50 -0300

    feat: add search component

---
+++ src/components/ui/Search/index.tsx
@@ -0,0 +1,14 @@
+import { FaSearch } from 'react-icons/fa';
+
+export default function Search() {
+  return (
+    <div className="relative w-full max-w-md">
+      <input
+        type="text"
+        placeholder="Informe o país que deseja conhecer..."
+        className="w-full rounded-full border-[3px] border-white px-4 py-2 text-black italic placeholder-black focus:outline-none focus:ring-2 font-semibold focus:ring-white"
+      />
+      <FaSearch className="absolute right-4 top-1/2 -translate-y-1/2 text-white w-4 h-4" />
+    </div>
+  );
+}
